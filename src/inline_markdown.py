63a71cb0735e9e4d3fb7b9b256e81b808d7ed8f6
luckyBambooBro <huypham008@hotmail.com>
2025-09-12 07:49:13 +0930

    corrected file structure for readability. up to ch3,l6

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,14 +1,27 @@
-import re
 from textnode import TextNode, TextType
+import re
 
-def extract_markdown_images(text):
-    matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
-    return matches
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        sections = old_node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError("invalid markdown, formatted section not closed")
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                node = TextNode(sections[i], TextType.TEXT)
+            elif i % 2 == 1:
+                node = TextNode(sections[i], text_type)
+            split_nodes.append(node)
+        new_nodes.extend(split_nodes)
+        return new_nodes
     
-def extract_markdown_links(text):
-    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
-    return matches
-
 def split_nodes_link(old_nodes):
     new_nodes = []
     for old_node in old_nodes:
@@ -57,6 +70,13 @@ def split_nodes_img(old_nodes):
             new_nodes.append(TextNode(text, TextType.TEXT))
     return new_nodes
                 
+def extract_markdown_images(text):
+    matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
+    
+def extract_markdown_links(text):
+    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
 
 # nodes = [TextNode(
 #     "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
@@ -74,3 +94,4 @@ def split_nodes_img(old_nodes):
 #     TextNode("and here's some italicised text", TextType.ITALIC)
 # ]
 # old_nodes = split_nodes_img(nodes)
+ 
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,30 @@
 import unittest
+from inline_markdown import (
+    split_nodes_delimiter, 
+    split_nodes_img, 
+    split_nodes_link, 
+    extract_markdown_links, 
+    extract_markdown_images
+)
+
 from textnode import TextNode, TextType
-from extract_MD_img_and_link import extract_markdown_images, extract_markdown_links, split_nodes_link, split_nodes_img
+
+class testSplitNodesDelimiter(unittest.TestCase):
+    def test_split_nodes_delimiter(self):
+        new_nodes = split_nodes_delimiter([TextNode("This is text with a `code block` word", TextType.TEXT)], "`", TextType.CODE)
+        self.assertEqual(new_nodes, [TextNode("This is text with a ", TextType.TEXT), TextNode('code block', TextType.CODE), TextNode(" word", TextType.TEXT)])
+
+    def test_split_nodes_delimiter_start_word(self):
+        new_nodes = split_nodes_delimiter([TextNode("`This` is text with a code block word", TextType.TEXT)], "`", TextType.CODE)
+        self.assertEqual(new_nodes, [TextNode("This", TextType.CODE), TextNode(' is text with a code block word', TextType.TEXT)])
+
+    def test_split_nodes_delimiter_end_word(self):
+        new_nodes = split_nodes_delimiter([TextNode("This is text with a code block `word`", TextType.TEXT)], "`", TextType.CODE)
+        self.assertEqual(new_nodes, [TextNode("This is text with a code block ", TextType.TEXT), TextNode('word', TextType.CODE)])
+
+    def test_split_nodes_delimiter_2_delimiter_sets(self):
+        new_nodes = split_nodes_delimiter([TextNode("This is text with a `code block` word ... and `another code block`", TextType.TEXT)], "`", TextType.CODE)
+        self.assertEqual(new_nodes, [TextNode("This is text with a ", TextType.TEXT), TextNode('code block', TextType.CODE), TextNode(" word ... and ", TextType.TEXT), TextNode("another code block", TextType.CODE)])
 
 class test_Extract_img_and_link_regexes(unittest.TestCase):
     def test_extract_markdown_images(self):

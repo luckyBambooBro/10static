847a3cbd574db91e65a488726ddd7e9456b5ee0d
luckyBambooBro <huypham008@hotmail.com>
2025-09-13 19:28:04 +0930

    up to ch4, l3

--- src/markdown_to_blocks.py
+++ src/markdown_to_blocks.py
@@ -1,3 +1,13 @@
+from enum import Enum
+
+class Blocktype(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+    
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     for i in range(len(blocks)):
@@ -5,17 +15,28 @@ def markdown_to_blocks(markdown):
     blocks = [b for b in blocks if b]
     return blocks
 
-
-
-blocks = markdown_to_blocks("""# This is a heading
-
-This is a paragraph of text. It has some **bold** and _italic_ words inside of it.
-
-
-
-
-- This is the first list item in a list block
-- This is a list item
-- This is another list item""")
-
-print(blocks)
+def block_to_block_type(markdown_block):
+    lines = markdown_block.split("\n")
+    if markdown_block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return Blocktype.HEADING
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return Blocktype.CODE
+    if markdown_block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return Blocktype.PARAGRAPH
+        return Blocktype.QUOTE
+    if markdown_block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return Blocktype.PARAGRAPH
+        return Blocktype.UNORDERED_LIST
+    if markdown_block.startswith("1. "):
+        counter = 1
+        for line in lines:
+            if not line.startswith(f"{counter}. "):
+                return Blocktype.PARAGRAPH
+            counter += 1
+        return Blocktype.ORDERED_LIST
+    return Blocktype.PARAGRAPH
+    
--- src/test_markdown_to_blocks.py
+++ src/test_markdown_to_blocks.py
@@ -1,7 +1,7 @@
 import unittest
-from markdown_to_blocks import markdown_to_blocks
+from markdown_to_blocks import markdown_to_blocks, Blocktype, block_to_block_type
 
-class test_markdown_to_blocks_class(unittest.TestCase):
+class TestMarkdownToBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
         md = """
 This is **bolded** paragraph
@@ -43,3 +43,12 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+class TestBlockToBlock(unittest.TestCase):
+    def test_block_to_block(self):
+        self.assertEqual(block_to_block_type("# My Favourite Books"), Blocktype.HEADING)
+        self.assertEqual(block_to_block_type("``` print(\"hello world\")\n```"), Blocktype.CODE)
+        self.assertEqual(block_to_block_type("> luck is when preparation meets opportunity"), Blocktype.QUOTE)
+        self.assertEqual(block_to_block_type("- bread\n- tomatoes\n- cucumber"), Blocktype.UNORDERED_LIST)
+        self.assertEqual(block_to_block_type("1. bread\n2. tomatoes\n3. cucumber"), Blocktype.ORDERED_LIST)
+        self.assertEqual(block_to_block_type("lorem ipsum"), Blocktype.PARAGRAPH)

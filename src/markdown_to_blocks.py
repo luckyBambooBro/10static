b4f1b177027273a5d90985fa1a9e2de46bc85672
luckyBambooBro <huypham008@hotmail.com>
2025-09-20 07:37:19 +0930

    put erverything on the same scripts as lesson soln. will redo my work now

--- src/markdown_to_blocks.py
+++ src/markdown_to_blocks.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from htmlnode import ParentNode, LeafNode 
 from inline_markdown import text_to_textnodes
-from textnode import text_node_to_html_node
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class Blocktype(Enum):
     PARAGRAPH = "paragraph"
@@ -40,3 +40,90 @@ def block_to_block_type(markdown_block):
         return Blocktype.ORDERED_LIST
     return Blocktype.PARAGRAPH
 
+def text_to_children(block, blocktype):
+    def list_processor(reformatted_list):
+            text_nodes = []
+            for text in reformatted_list:
+                text_nodes.extend(text_to_textnodes(text))
+            return [text_node_to_html_node(text_node) for text_node in text_nodes]
+    
+    match blocktype:
+        case Blocktype.PARAGRAPH | Blocktype.HEADING | Blocktype.QUOTE:
+            text_nodes = text_to_textnodes(block)
+            return [text_node_to_html_node(text_node) for text_node in text_nodes] 
+        
+        case Blocktype.CODE:
+            return [text_node_to_html_node(TextNode(block, TextType.CODE))]
+
+        case Blocktype.UNORDERED_LIST:
+            reformatted_list = [text.replace("- ", "<li>") + "</li>" for text in block.split("\n")]
+            return list_processor(reformatted_list)
+        
+        case Blocktype.ORDERED_LIST:
+            reformatted_list = ["<li>" + text[3:] + "</li>" for text in block.split("\n")]
+            return list_processor(reformatted_list)
+
+        case _:
+            raise TypeError("blocktype must be a Blocktype class object")
+
+
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    parent_nodes = []
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        match block_type:
+            case Blocktype.PARAGRAPH:
+                block = block.replace("\n", "")
+                child_nodes = text_to_children(block, Blocktype.PARAGRAPH)
+                parent_node = ParentNode("p", child_nodes)
+                parent_nodes.append(parent_node)
+                
+            case Blocktype.HEADING:
+                level = 0
+                for i in block[:6]:
+                    if i == "#":
+                        level += 1
+                if not 1 <= level <= 6:
+                    raise ValueError("Heading must be between <h1> to <h6>")
+                child_nodes = text_to_children(block[level + 1: ], Blocktype.HEADING)
+                parent_node = ParentNode(f"h{level}", child_nodes)
+                parent_nodes.append(parent_node)
+
+            case Blocktype.CODE:
+                block = block.strip("```")
+                block = block.lstrip("\n")
+                child_nodes = text_to_children(block, Blocktype.CODE)
+                parent_node = ParentNode("pre", child_nodes)
+                parent_nodes.append(parent_node)
+
+            case Blocktype.QUOTE:
+                block = block.strip(">")
+                child_nodes = text_to_children(block, Blocktype.QUOTE) 
+                parent_node = ParentNode("blockquote", child_nodes)
+                parent_nodes.append(parent_node)
+
+            case Blocktype.UNORDERED_LIST:
+                child_nodes = text_to_children(block, Blocktype.UNORDERED_LIST) 
+                parent_node = ParentNode("ul", child_nodes)
+                parent_nodes.append(parent_node)
+                           
+            case Blocktype.ORDERED_LIST:
+                child_nodes = text_to_children(block, Blocktype.ORDERED_LIST) 
+                parent_node = ParentNode("ol", child_nodes)
+                parent_nodes.append(parent_node)
+    root_parent_node = ParentNode("div", parent_nodes)
+    return root_parent_node
+
+
+
+md_text = markdown_to_html_node("""
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+""")
+# print(md_text.to_html())
--- src/test_markdown_to_blocks.py
+++ src/test_markdown_to_blocks.py
@@ -1,5 +1,9 @@
 import unittest
-from markdown_to_blocks import markdown_to_blocks, Blocktype, block_to_block_type
+from markdown_to_blocks import (
+    markdown_to_blocks, 
+    Blocktype, 
+    block_to_block_type, 
+    markdown_to_html_node)
 
 class TestMarkdownToBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -52,3 +56,36 @@ class TestBlockToBlock(unittest.TestCase):
         self.assertEqual(block_to_block_type("- bread\n- tomatoes\n- cucumber"), Blocktype.UNORDERED_LIST)
         self.assertEqual(block_to_block_type("1. bread\n2. tomatoes\n3. cucumber"), Blocktype.ORDERED_LIST)
         self.assertEqual(block_to_block_type("lorem ipsum"), Blocktype.PARAGRAPH)
+
+class test_md_to_html(unittest.TestCase):
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph text 
+in a p 
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+        )

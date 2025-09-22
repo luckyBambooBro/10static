a242010997c7e6ff6e30962f03a81f2b1ba6b5b7
luckyBambooBro <huypham008@hotmail.com>
2025-09-22 18:47:38 +0930

    up to ch5, l2

--- src/main.py
+++ src/main.py
@@ -8,17 +8,20 @@ def copy_src_to_dst(src, dst):
         for item in os.listdir(src):
             item_path = os.path.join(src,item)
             if os.path.isfile(item_path):
+                print(f"copying file: {item_path}")
                 shutil.copy(item_path, dst)
             else:
                 src_subdir_path = os.path.join(src,item)
                 dst_subdir_path = os.path.join(dst,item)
                 if not os.path.exists(dst_subdir_path):
+                    print(f"creating folder: {dst_subdir_path}")
                     os.mkdir(dst_subdir_path)
                 copy_contents(src_subdir_path, dst_subdir_path)
-
     shutil.rmtree(os.path.abspath(dst))
     os.mkdir(dst)
     copy_contents(src, dst)
 
-copy_src_to_dst(src, dst)
+
+if __name__ == "__main__":
+    copy_src_to_dst(src, dst)
 

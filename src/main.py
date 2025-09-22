708a3aeddc78d4157086620f41d1ee7d8064b24e
luckyBambooBro <huypham008@hotmail.com>
2025-09-22 15:41:30 +0930

    mid chapter

--- src/main.py
+++ src/main.py
@@ -1,31 +1,20 @@
-import os, shutil
+import os, pprint, shutil
 
 source = "static"
 dest = "public"
 
-def copy_contents(source, dest):
-    shutil.rmtree(os.path.join(dest))
+def copy_contents(source, dest, paths=[]):
+    shutil.rmtree(os.path.abspath(dest))
     os.mkdir(dest)
-    src_paths = obtain_src_paths(source)
-    for item in src_paths:
-        shutil.copy(item, dest)
-    print(src_paths)
-    #TODO: so far my function just copies the files straight into dst. i need to 
-    # make it so that it copies the full file path including the subdirectories that 
-    # contain the file#
-
-
-def obtain_src_paths(source, paths=[]):
-    dest_contents = os.listdir(source)
-    if not dest_contents:
-        paths.append(source)
-        return
-    for item in dest_contents:
-        item_path = os.path.join(source, item)
-        if os.path.isfile(item_path):
-            paths.append(item_path)
+    
+    for item in os.listdir(source):
+        #TODO up to here but this is a good start
+        if os.path.isfilepath(item):
+            paths.append(item)
         else:
-            obtain_src_paths(os.path.join(source, item), paths)
-    return paths
+            if not os.path.exists(item):
+                os.mkdir(os.path.join(dest, item))
+                #   TODO continue
+
 
 copy_contents(source, dest)

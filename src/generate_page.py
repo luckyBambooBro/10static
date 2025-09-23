a30677c935a014699c0fe6eeb622ac2238b13ae8
luckyBambooBro <huypham008@hotmail.com>
2025-09-24 07:10:55 +0930

    up to 5.5 working on generate_page.py

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,3 +1,6 @@
+from markdown_to_blocks import markdown_to_html_node
+import pprint
+
 def extract_title(markdown):
     title = markdown.split("# ")[1]
     title = title.split("\n\n")[0]
@@ -6,9 +9,14 @@ def extract_title(markdown):
 def generate_page(from_path, template_path, dst_path):
     print(f"Generating page from {from_path} to {dst_path} using template_path")
     with open(from_path, "r") as from_path_f:
-        md_text = from_path_f.read()
+        md_text_str = from_path_f.read()
     with open(template_path, "r") as template_path_f:
         html_template = template_path_f.read()
+    html_nodes = markdown_to_html_node(md_text_str)
+    html_str = html_nodes.to_html()
+    print(html_str)
+
+    
 
 
 

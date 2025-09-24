8f03f5b6c9ad47cfe148a9f770d917c201228ad9
Huy Pham <huypham@Adas-MacBook-Pro.local>
2025-09-24 16:46:38 +0930

    up to 5.6

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,5 +1,5 @@
 from markdown_to_blocks import markdown_to_html_node
-import pprint
+import os
 
 def extract_title(markdown):
     title = markdown.split("# ")[1]
@@ -8,19 +8,18 @@ def extract_title(markdown):
 
 def generate_page(from_path, template_path, dst_path):
     print(f"Generating page from {from_path} to {dst_path} using template_path")
+
     with open(from_path, "r") as from_path_f:
         md_text_str = from_path_f.read()
     with open(template_path, "r") as template_path_f:
         html_template = template_path_f.read()
+
     html_nodes = markdown_to_html_node(md_text_str)
     html_str = html_nodes.to_html()
-    print(html_str)
+    title = extract_title(md_text_str)
+    html_contents = html_template.replace("{{ Title }}", title).replace("{{ Content }}", html_str)
+    print(html_contents)
+    with open(os.path.join(dst_path, "html_page.html"), "w") as html_f:
+        html_file = html_f.write(html_contents)
 
-    
-
-
-
-
-
-
-generate_page("./content/index.md", "template.html", "public")
+generate_page("./content/index.md", "./template.html", "./public")

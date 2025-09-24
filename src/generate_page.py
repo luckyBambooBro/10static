ed77efd84562f72d167c551948b7b5dea3fff927
luckyBambooBro <huypham008@hotmail.com>
2025-09-24 21:25:13 +0930

    up to step 6, just testing my work first before continuing to 7

--- src/generate_page.py
+++ src/generate_page.py
@@ -7,19 +7,21 @@ def extract_title(markdown):
     return title
 
 def generate_page(from_path, template_path, dst_path):
-    print(f"Generating page from {from_path} to {dst_path} using template_path")
+    print(f"Generating page from {from_path} to {dst_path} using {template_path}")
 
-    with open(from_path, "r") as from_path_f:
-        md_text_str = from_path_f.read()
     with open(template_path, "r") as template_path_f:
         html_template = template_path_f.read()
 
-    html_nodes = markdown_to_html_node(md_text_str)
-    html_str = html_nodes.to_html()
-    title = extract_title(md_text_str)
-    html_contents = html_template.replace("{{ Title }}", title).replace("{{ Content }}", html_str)
-    print(html_contents)
+    with open(from_path, "r") as from_path_f:
+        md_text_str = from_path_f.read()
+        html_str = markdown_to_html_node(md_text_str).to_html()
+        html_contents = html_template.replace(
+        "{{ Title }}", extract_title(md_text_str)).replace(
+        "{{ Content }}", html_str)
+
     with open(os.path.join(dst_path, "html_page.html"), "w") as html_f:
         html_file = html_f.write(html_contents)
+        print("...")
+        print(f"{from_path} successfully written to {dst_path} ")
 
-generate_page("./content/index.md", "./template.html", "./public")
+
--- src/main.py
+++ src/main.py
@@ -1,18 +1,20 @@
 import os, shutil
 from copy_static import copy_src_to_dst
 from markdown_to_blocks import markdown_to_html_node
-from generate_page import extract_title
+from generate_page import extract_title, generate_page
 
 src = "./static"
 dst = "./public"
 
 extract_title("# Hello")
 
-# if __name__ == "__main__":
-#     print(f"deleting {dst} directory...")
-#     if os.path.exists(os.path.abspath(dst)):
-#         shutil.rmtree(os.path.abspath(dst))
-#     os.mkdir(dst)
-#     print(f"copying {src} --> {dst}")
-#     copy_src_to_dst(src, dst)
+if __name__ == "__main__":
+    print(f"deleting {dst} directory...")
+    if os.path.exists(os.path.abspath(dst)):
+        shutil.rmtree(os.path.abspath(dst))
+    os.mkdir(dst)
+    print(f"copying {src} --> {dst}")
+    copy_src_to_dst(src, dst)
+
+    generate_page("./content/index.md", "./template.html", "./public")
 

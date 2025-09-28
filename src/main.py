1bd5ec4a171adec2c3b7ee50d1c9a864e9793cb4
luckyBambooBro <huypham008@hotmail.com>
2025-09-29 08:51:12 +0930

    ch5,l5 p2

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import os, shutil
+import os, shutil, sys
 from copy_static import copy_src_to_dst
 from generate_page import generate_pages_recursive
 
@@ -6,8 +6,12 @@ src = "./static"
 dst = "./public"
 template_path = "./template.html"
 dir_path_content = "./content"
+basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+
+
 
 if __name__ == "__main__":
+    print(basepath)
     print(f"deleting {dst} directory...")
     if os.path.exists(os.path.abspath(dst)):
         shutil.rmtree(os.path.abspath(dst))
@@ -20,5 +24,5 @@ if __name__ == "__main__":
         dir_path_content, 
         template_path, 
         dst)
-    # generate_pages_recursive()
+
 
